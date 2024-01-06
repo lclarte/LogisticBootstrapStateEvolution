7e782d708217c53da909ebc507563e7da64f38a0
Lucas Clarte <lucas.clarte@epfl.ch>
2024-01-06 23:01:29 +0100

    - new version : now only compute the off diagonal terms, much faster now

--- src/LogisticBootstrapStateEvolution.jl
+++ src/LogisticBootstrapStateEvolution.jl
@@ -14,43 +14,43 @@ export state_evolution
 const Bound = 7.5
 const LogisticProbitFactor = 0.5875651988237005
 
+sigmoid(x::Number) = logistic(x)
+
 function weights_proba_function_bootstrap(w1::Number, w2::Number)
     return poispdf(1, w1) * poispdf(1, w2)
 end
 
-sigmoid(x::Number) = logistic(x)
-
-function gradient_logistic_loss(y::Number, z::AbstractVector, weights::AbstractVector)
-    return weights .* (sigmoid.(y .* z) .- 1) .* y
+function gradient_logistic_loss(y::Number, z::Number)
+    return (sigmoid(y * z) - 1) * y
 end
 
-function hessian_logistic_loss(z::AbstractVector, weights::AbstractVector)
-    return diagm(weights ./ (cosh.(z ./ 2.0) .^ 2 .* 4.0))
+function hessian_logistic_loss(z::Number)
+    return 1.0 ./ (cosh.(z ./ 2.0) .^ 2 .* 4.0)
 end
 
-function prox_logistic_multivariate(
-    y::Number, omega::AbstractVector, v_inv::AbstractMatrix, weights::AbstractVector
+function prox_logistic_univariate(
+    y::Number, omega::Number, v::Number, weight::Number
 )
-    if all(iszero, weights)
+    if weight == 0.0
         return omega
     end
 
-    function objective(z::AbstractVector)
-        a = dot(weights, log1p.(exp.(-y .* z)))
-        b = dot(z - omega, v_inv * (z - omega)) / 2.0
+    function objective(z::Number)
+        a = weight * log1p(exp(-y * z))
+        b = (z - omega)^2 / (2.0 * v)
         return a + b
     end
 
-    function gradient(g::AbstractVector, z::AbstractVector)
-        ga = gradient_logistic_loss(y, z, weights)
-        gb = v_inv * (z - omega)
+    function gradient(g::Number, z::Number)
+        ga = weight * gradient_logistic_loss(y, z)
+        gb = (z - omega) / v
         g = ga + gb
         return g
     end
 
-    function hessian(H::AbstractMatrix, z::AbstractVector)
-        Ha = hessian_logistic_loss(z, weights)
-        Hb = v_inv
+    function hessian(H::Number, z::Number)
+        Ha = weight * hessian_logistic_loss(z)
+        Hb = 1.0 / v
         H = Ha + Hb
         return H
     end
@@ -70,22 +70,21 @@ function prox_logistic_multivariate(
     return res.info.solution
 end
 
-function gout_logistic_multivariate(
-    y::Number, omega::AbstractVector, v_inv::AbstractMatrix, weights::AbstractVector
+function gout_logistic_univariate(
+    y::Number, omega::Number, v::Number, weight::Number
 )
-    return v_inv * (prox_logistic_multivariate(y, omega, v_inv, weights) - omega)
+    return (prox_logistic_univariate(y, omega, v, weight) - omega) / v
 end
 
-function dwgout_logistic_multivariate(
+function dwgout_logistic_univariate(
     y::Number,
-    omega::AbstractVector,
-    v_inv::AbstractMatrix,
-    weights::AbstractVector,
-    v::AbstractMatrix,
+    omega::Number,
+    v::Number,
+    weight::Number,
 )
-    prox::MVector{2} = prox_logistic_multivariate(y, omega, v_inv, weights)
-    derivative_prox = inv(I + v * hessian_logistic_loss(prox, weights))
-    return v_inv * (derivative_prox - I)
+    prox::MVector{2} = prox_logistic_univariate(y, omega, v, weight)
+    derivative_prox = 1.0 / (1.0 + v * weight * hessian_logistic_loss(prox))
+    return (derivative_prox - 1.0) / v
 end
 
 function logistic_z0(y::Number, mean::Number, variance::Number)
@@ -114,24 +113,24 @@ function integrand_qhat(
     y::Number,
     m::AbstractVector,
     q_sqrt::AbstractMatrix,
-    q_inv_sqrt::AbstractMatrix,
-    v_inv::AbstractMatrix,
+    q_inv::AbstractMatrix,
+    v::AbstractVector,
     v_star_float,
     weights::AbstractVector,
 )
     omega = q_sqrt * x
-    conditional_mean = m' * q_inv_sqrt * x
-    g = gout_logistic_multivariate(y, omega, v_inv, weights)
-    return logistic_z0_approximate(y, conditional_mean, v_star_float) * g * g'
+    conditional_mean = m' * q_inv * omega
+    g1 = gout_logistic_univariate(y, omega[1], v[1], weights[1])
+    g2 = gout_logistic_univariate(y, omega[2], v[2], weights[2])
+    return logistic_z0_approximate(y, conditional_mean, v_star_float) * g1 * g2
 end
 
 function update_qhat(
-    m::AbstractVector, q::AbstractMatrix, v::AbstractMatrix, rho, max_weight=2
+    m::AbstractVector, q::AbstractMatrix, v::AbstractVector, rho, max_weight=2
 )
-    result = SMatrix{2,2}(zeros(2, 2))
+    result = 0.0
     q_sqrt = sqrt(q)
-    q_inv_sqrt = inv(q_sqrt)
-    v_inv = inv(v)
+    q_inv  = inv(q)
     q_inv = inv(q)
     v_star_float = rho - m' * q_inv * m
 
@@ -141,7 +140,7 @@ function update_qhat(
             for label in (-1, 1)
                 function fq(x, p)
                     a = integrand_qhat(
-                        x, label, m, q_sqrt, q_inv_sqrt, v_inv, v_star_float, weights
+                        x, label, m, q_sqrt, q_inv, v, v_star_float, weights
                     )
                     b = prod(normpdf, x)
                     return a * b
@@ -158,154 +157,35 @@ function update_qhat(
     return result
 end
 
-## 
+##  
 
-function integrand_mhat(
-    x::AbstractVector,
-    y::Number,
-    m::AbstractVector,
-    q_sqrt::AbstractMatrix,
-    q_inv_sqrt::AbstractMatrix,
-    v_inv::AbstractMatrix,
-    v_star_float::Number,
-    weights::AbstractVector,
+function update_q(
+    mhat::AbstractVector, qhat::AbstractMatrix, vhat::AbstractVector, lambda::Number
 )
-    omega = q_sqrt * x
-    conditional_mean = m' * q_inv_sqrt * x
-    g = gout_logistic_multivariate(y, omega, v_inv, weights)
-    return logistic_dz0(y, conditional_mean, v_star_float) * g
-end
-
-function update_mhat(
-    m::AbstractVector, q::AbstractMatrix, v::AbstractMatrix, rho, max_weight=2
-)
-    result = SVector{2}(zeros(2))
-    q_sqrt = sqrt(q)
-    q_inv = inv(q)
-    q_inv_sqrt = inv(q_sqrt)
-    v_inv = inv(v)
-    v_star_float = rho - m' * q_inv * m
-
-    for w1 in 0:(max_weight - 1)
-        for w2 in 0:(max_weight - 1)
-            weights = SVector(w1, w2)
-            for label in (-1, 1)
-                function fm(x, p)
-                    a = integrand_mhat(
-                        x, label, m, q_sqrt, q_inv_sqrt, v_inv, v_star_float, weights
-                    )
-                    b = prod(normpdf, x)
-                    return a * b
-                end
-                prob = Integrals.IntegralProblem(
-                    fm, SVector(-Bound, -Bound), SVector(Bound, Bound)
-                )
-                sol = Integrals.solve(prob, HCubatureJL(); reltol=1e-3)
-                result += sol.u * weights_proba_function_bootstrap(w1, w2)
-            end
-        end
-    end
-
-    return result
-end
-
-# 
-
-function integrand_vhat(
-    x::AbstractVector,
-    y::Number,
-    m::AbstractVector,
-    q_sqrt::AbstractMatrix,
-    q_inv_sqrt::AbstractMatrix,
-    v::AbstractMatrix,
-    v_inv::AbstractMatrix,
-    v_star_float,
-    weights::AbstractVector,
-)
-    omega = q_sqrt * x
-    conditional_mean = m' * q_inv_sqrt * x
-    dg = dwgout_logistic_multivariate(y, omega, v_inv, weights, v)
-    return logistic_z0_approximate(y, conditional_mean, v_star_float) * dg
-end
-
-function update_vhat(
-    m::AbstractVector, q::AbstractMatrix, v::AbstractMatrix, rho, max_weight=2
-)
-    result = SMatrix{2,2}(zeros(2, 2))
-    q_sqrt = sqrt(q)
-    q_inv = inv(q)
-    q_inv_sqrt = inv(q_sqrt)
-    v_inv = inv(v)
-    v_star_float = rho - m' * q_inv * m
-
-    for w1 in 0:(max_weight - 1)
-        for w2 in 0:(max_weight - 1)
-            weights = SVector(w1, w2)
-            for label in (-1, 1)
-                function fv(x, p)
-                    a = integrand_vhat(
-                        x, label, m, q_sqrt, q_inv_sqrt, v, v_inv, v_star_float, weights
-                    )
-                    b = prod(normpdf, x)
-                    return a * b
-                end
-                prob = Integrals.IntegralProblem(
-                    fv, SVector(-Bound, -Bound), SVector(Bound, Bound)
-                )
-                sol = Integrals.solve(prob, HCubatureJL(); reltol=1e-3)
-                result += sol.u * weights_proba_function_bootstrap(w1, w2)
-            end
-        end
-    end
-    return result
-end
-
-## functions to update the overlap
-
-function update_overlaps(
-    mhat::AbstractVector, qhat::AbstractMatrix, vhat::AbstractMatrix, lambda::Number
-)
-    tmp = inv(lambda * I + vhat)
-    m = tmp * mhat
-    q = tmp * (mhat * mhat' + qhat) * tmp'
-    v = tmp
-
-    return m, q, v
+    tmp = diagm( 1.0 ./ (lambda .+ vhat))
+    return tmp * (mhat * mhat' .+ qhat) * tmp
 end
 
 function state_evolution(
-    sampling_ratio, regularisation, max_weight=2; relative_tolerance=1e-4, max_iteration=100
+    m::AbstractVector, qdiag::AbstractVector, v::AbstractVector, mhat::AbstractVector, qhatdiag::AbstractVector, vhat::AbstractVector, sampling_ratio::Number, regularisation::Number, max_weight::Number=5; max_iteration=2
 )
     rho = 1.0
 
-    old_m = SVector(0.0, 0.0)
-    m = SVector(0.0, 0.0)
-    q = SMatrix{2,2}([1.0 0.01; 0.01 1.0])
-    v = SMatrix{2,2}([1.0 0.01; 0.01 1.0])
-
-    mhat = SVector(0.0, 0.0)
-    qhat = SMatrix{2,2}([1.0 0.01; 0.01 1.0])
-    vhat = SMatrix{2,2}([1.0 0.01; 0.01 1.0])
+    q = MMatrix{2,2}(diagm(qdiag))
+    qhat = MMatrix{2,2}(diagm(qhatdiag))
 
     for i in 0:max_iteration
         # copy m into old_m to compute the difference at the end of the loop
-        old_m = copy(m)
 
-        mhat = sampling_ratio * update_mhat(m, q, v, rho, max_weight)
-        qhat = sampling_ratio * update_qhat(m, q, v, rho, max_weight)
-        vhat = sampling_ratio * update_vhat(m, q, v, rho, max_weight)
+        update = sampling_ratio * update_qhat(m, q, v, rho, max_weight)
+        qhat[1, 2] = update
+        qhat[2, 1] = update
 
-        m, q, v = update_overlaps(mhat, qhat, vhat, regularisation)
-
-        # compute the relative difference between old and new m 
-        difference = norm(m - old_m) / norm(m)
-        if difference < relative_tolerance
-            return m, q, v, mhat, qhat, vhat
-        end
+        q    = update_q(mhat, qhat, vhat, regularisation)
     end
 
     println("Warning: state evolution did not converge after $max_iteration iterations")
-    return m, q, v, mhat, qhat, vhat
+    return q, qhat
 end
 
 end  # module
--- test/scratchpad.jl
+++ test/scratchpad.jl
@@ -1,10 +1,33 @@
 using LogisticBootstrapStateEvolution
+using StaticArrays
 
+#= 
 alpha  = 1.0
 lambda = 2.0
 
-result_1 = state_evolution(alpha, lambda, 2, max_iteration=10);
-@time result = state_evolution(alpha, lambda, 6, max_iteration=20);
+# hardcoded values for this value of alpha = 1.0 and lambda = 2.0
+m        = SVector{2}([ 0.07756231047458477, 0.07756231047458477 ] )
+qdiag    = SVector{2}([ 0.07034273363434002, 0.07034273363434002 ])
+v        = SVector{2}([ 0.4551180048548463 , 0.4551180048548463  ])
+mhat     = SVector{2}([ 0.17042241714722361, 0.17042241714722361 ])
+qhatdiag = SVector{2}([ 0.31055895718276305, 0.31055895718276305 ])
+vhat     = SVector{2}([ 0.19723234267327278, 0.19723234267327278 ])
+=#
+
+alpha = 2.0
+lambda = 0.1
+
+m        = SVector{2}([0.7629706003134091,  0.7629706003134091])
+qdiag    = SVector{2}([3.8746065374638277,  3.8746065374638277])
+v        = SVector{2}([4.447796051796358,   4.447796051796358])
+mhat     = SVector{2}([0.1715390254922466,  0.1715390254922466])
+qhatdiag = SVector{2}([0.16643081204981966, 0.16643081204981966])
+vhat     = SVector{2}([0.12483045273537757, 0.12483045273537757])
+
+
+result_1 = state_evolution(m, qdiag, v, mhat, qhatdiag, vhat, alpha, lambda, 6, max_iteration=30);
+result_1
+@time state_evolution(m, qdiag, v, mhat, qhatdiag, vhat, alpha, lambda, 6, max_iteration=10);
 result
-@profview state_evolution(alpha, lambda, 3, max_iteration=5)
+@profview state_evolution(m, qdiag, v, mhat, qhatdiag, vhat, alpha, lambda, 6, max_iteration=10)
 @profview_allocs state_evolution(alpha, lambda, 3, max_iteration=5)
